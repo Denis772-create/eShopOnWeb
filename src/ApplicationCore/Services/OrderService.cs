be875bdb74c144c3dc728e05f29c075bd1b31f86
Aliaksandrou, Dzianis <D.Aliaksandrou@wolterskluwer.com>
2022-09-25 01:32:36 +0500

    feat: added some changes according to task from course CloudX

--- src/ApplicationCore/Services/OrderService.cs
+++ src/ApplicationCore/Services/OrderService.cs
@@ -1,11 +1,21 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Ardalis.GuardClauses;
+using Azure.Messaging.ServiceBus;
+using BlazorShared;
+using Microsoft.Azure.Amqp;
 using Microsoft.eShopWeb.ApplicationCore.Entities;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 using Microsoft.eShopWeb.ApplicationCore.Specifications;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.eShopWeb.ApplicationCore.Services;
 
@@ -15,16 +25,30 @@ public class OrderService : IOrderService
     private readonly IUriComposer _uriComposer;
     private readonly IRepository<Basket> _basketRepository;
     private readonly IRepository<CatalogItem> _itemRepository;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly BaseUrlConfiguration _urls;
+    private readonly IConfiguration _configuration;
+    private readonly ServiceBusClient _serviceBusClient;
+    private readonly ServiceBusSender _busSender;
 
     public OrderService(IRepository<Basket> basketRepository,
         IRepository<CatalogItem> itemRepository,
         IRepository<Order> orderRepository,
-        IUriComposer uriComposer)
+        IUriComposer uriComposer,
+        IHttpClientFactory httpClientFactory,
+        IOptions<BaseUrlConfiguration> options,
+        IConfiguration configuration)
     {
         _orderRepository = orderRepository;
         _uriComposer = uriComposer;
         _basketRepository = basketRepository;
         _itemRepository = itemRepository;
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+        _serviceBusClient = new ServiceBusClient(_configuration["ServiseBus"],
+            new ServiceBusClientOptions { TransportType = ServiceBusTransportType.AmqpTcp });
+        _busSender = _serviceBusClient.CreateSender("eshop");
+        _urls = options.Value;
     }
 
     public async Task CreateOrderAsync(int basketId, Address shippingAddress)
@@ -49,5 +73,79 @@ public class OrderService : IOrderService
         var order = new Order(basket.BuyerId, shippingAddress, items);
 
         await _orderRepository.AddAsync(order);
+
+        await SendToWarehouseAsync(order.Id, items);
+
+        await SendToDeliveryProcessor(shippingAddress, items, order.Total());
     }
+
+    private async Task SendToDeliveryProcessor(Address shippingAddress,
+        List<OrderItem> items, decimal price)
+    {
+        var deliveryInfoDto = new DeliveryInfoDto(price, shippingAddress, items);
+
+        var httpClient = _httpClientFactory.CreateClient();
+        var result = await httpClient
+            .PostAsync($"{_urls.DeliveryProcessorFunc}", 
+                JsonContent.Create(deliveryInfoDto));
+
+        result.EnsureSuccessStatusCode();
+    }
+
+    private class DeliveryInfoDto
+    {
+        public decimal Price { get; set; }
+        public Address ShippingAddress { get; set; }
+        public List<OrderItem> Items { get; set; }  
+
+        public DeliveryInfoDto(decimal price, Address shippingAddress, List<OrderItem> items)
+        {
+            Price = price;
+            ShippingAddress = shippingAddress;
+            Items = items;
+        }
+
+        public DeliveryInfoDto()
+        {
+
+        }
+    }
+
+    private async Task SendToWarehouseAsync(int orderId, List<OrderItem> items)
+    {
+        var orderItemsDto = new List<OrderItemDto>();
+        items.ForEach(x => orderItemsDto.Add(new OrderItemDto
+        {
+            Id = x.Id,
+            Name = x.ItemOrdered.ProductName,
+            Units = x.Units
+        }));
+
+        await _busSender.SendMessageAsync(
+            new ServiceBusMessage(
+                    JsonSerializer.Serialize(new OrderToWarehouse(orderItemsDto, orderId))));
+    }
+
+    private class OrderToWarehouse
+    {
+        public int OrderId { get; set; }
+        public List<OrderItemDto> Items { get; set; }
+
+        public OrderToWarehouse(List<OrderItemDto> items,
+            int orderId)
+        {
+            Items = items;
+            OrderId = orderId;
+        }
+    }
+
+    private class OrderItemDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Units { get; set; }
+    }
+
+
 }
+
